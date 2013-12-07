82c25951cd6355c3b917c1bda4f9a320fc337717
Jason Roth <jasonmichaelroth@gmail.com>
2013-12-07 10:38:55 -0700

    Now in Clojure!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Puzzle Node
+
+My solutions to the Puzzle Node problems in clojure.
+
+http://www.puzzlenode.com/puzzles
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject puzzlenode "1.0.0"
+  :description "Puzzle Node Problems"
+  :url "http://www.puzzlenode.com/puzzles"
+  :license {:name "WFTPL"
+            :url "http://www.wtfpl.net"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
